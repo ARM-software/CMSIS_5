23288b7a7a1150502d926d86042e0b69f3edf893
Matthias Hertel <Matthias.Hertel@arm.com>
2016-07-22 15:18:56 +0200

    Initial CMSIS-RTOS v2 API Documentation, Example and Component Viewer Description

--- CMSIS/DoxyGen/Doxygen_Templates/_cp_search.bat
+++ CMSIS/DoxyGen/Doxygen_Templates/_cp_search.bat
@@ -4,4 +4,5 @@ copy /Y search.css ..\Driver\html\search\.
 copy /Y search.css ..\DSP\html\search\. 
 copy /Y search.css ..\Pack\html\search\. 
 copy /Y search.css ..\RTOS\html\search\. 
+copy /Y search.css ..\RTOS2\html\search\. 
 copy /Y search.css ..\SVD\html\search\. 
