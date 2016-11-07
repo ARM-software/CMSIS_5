d94714b3a4fcef966c430852e283f39a3bd50ddf
ReinhardKeil <reinhard.keil@arm.com>
2016-11-07 10:37:47 +0100

    TrustZone: Cortex-M33 NoRTOS example added

---
+++ CMSIS/RTOS2/RTX/Examples/TrustZoneV8M/NoRTOS/CM33_s/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ARMv8MBL_s' 
+ * Target:  'FVP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMv8MBL.h"
+
+
+#endif /* RTE_COMPONENTS_H */
