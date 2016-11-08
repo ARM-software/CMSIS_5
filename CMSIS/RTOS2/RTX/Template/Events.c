7b6073fa5a66ce35c0c45c0a468cd0a87f08793a
Matthias Hertel <Matthias.Hertel@arm.com>
2016-11-08 14:06:25 +0100

    RTOS2 Doc

--- CMSIS/RTOS2/RTX/Template/Events.c
+++ CMSIS/RTOS2/RTX/Template/Events.c
@@ -10,19 +10,6 @@ void Thread_EventReceiver (void *argument);                   // thread function
 osThreadId_t tid_Thread_EventSender;                                // thread id 1
 osThreadId_t tid_Thread_EventReceiver;                                // thread id 2
 
-#define MSGQUEUE_OBJECTS      16                                // number of Message Queue Objects
-
-typedef struct {                                                // object data type
-  uint8_t Buf[32];
-  uint8_t Idx;
-} MEM_BLOCK_t;
-
-typedef struct {                                                // object data type
-  uint8_t Buf[32];
-  uint8_t Idx;
-} MSGQUEUE_OBJ_t;
-
-
 osEventFlagsId_t evt_id;                                      // message queue id
 
 #define FLAGS_MSK1 0x00000001ul
