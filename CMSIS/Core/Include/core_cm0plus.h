f9380f8a5d9d01b9976b117ed825353401ac93fa
Jaeden Amero <jaeden.amero@arm.com>
2017-02-09 14:34:18 +0000

    core: Allow virtualization of NVIC_SystemReset
    Previously, we only allowed the virtualization on NVIC_SystemReset on
    select architectures. Add NVIC_SystemReset() to the set of functions
    that can be virtualized via CMSIS_NVIC_VIRTUAL on all architectures.

--- CMSIS/Core/Include/core_cm0plus.h
+++ CMSIS/Core/Include/core_cm0plus.h
@@ -697,6 +697,7 @@ typedef struct
 /*#define NVIC_GetActive              __NVIC_GetActive             not available for Cortex-M0+ */
   #define NVIC_SetPriority            __NVIC_SetPriority
   #define NVIC_GetPriority            __NVIC_GetPriority
+  #define NVIC_SystemReset            __NVIC_SystemReset
 #endif /* CMSIS_NVIC_VIRTUAL */
 
 #ifdef CMSIS_VECTAB_VIRTUAL
@@ -915,7 +916,7 @@ __STATIC_INLINE uint32_t __NVIC_GetVector(IRQn_Type IRQn)
   \brief   System Reset
   \details Initiates a system reset request to reset the MCU.
  */
-__STATIC_INLINE void NVIC_SystemReset(void)
+__STATIC_INLINE void __NVIC_SystemReset(void)
 {
   __DSB();                                                          /* Ensure all outstanding memory accesses included
                                                                        buffered write are completed before reset */
