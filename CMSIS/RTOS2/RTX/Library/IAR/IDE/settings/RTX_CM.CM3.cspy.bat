97c25ae7c82a6bccda4d128132f3db7ef4b13466
Vladimir Umek <vladimir.umek@arm.com>
2017-06-12 13:09:00 +0200

    RTX5: updated library project and libraries (IAR)

--- CMSIS/RTOS2/RTX/Library/IAR/IDE/settings/RTX_CM.CM3.cspy.bat
+++ CMSIS/RTOS2/RTX/Library/IAR/IDE/settings/RTX_CM.CM3.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\SupportGroup\Desktop\Julian\newStat\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.general.xcl" --backend -f "C:\Users\SupportGroup\Desktop\Julian\newStat\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\GitHub\ARM-software\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.general.xcl" --backend -f "D:\GitHub\ARM-software\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\SupportGroup\Desktop\Julian\newStat\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\SupportGroup\Desktop\Julian\newStat\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\GitHub\ARM-software\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.general.xcl" "--debug_file=%~1" --backend -f "D:\GitHub\ARM-software\CMSIS_5\CMSIS\RTOS2\RTX\Library\IAR\IDE\settings\RTX_CM.CM3.driver.xcl" 
 
 @echo off 
 :end
