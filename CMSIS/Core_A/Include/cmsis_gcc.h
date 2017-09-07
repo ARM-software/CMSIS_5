ca5cf5dba4c9d6245d91bd28ecc0c8f0873c3f48
Jonatan Antoni <jonatan.antoni@arm.com>
2017-09-07 16:21:48 +0200

    Fixed documentation after Core(A) refactoring.

--- CMSIS/Core_A/Include/cmsis_gcc.h
+++ CMSIS/Core_A/Include/cmsis_gcc.h
@@ -1,8 +1,8 @@
 /**************************************************************************//**
  * @file     cmsis_gcc.h
  * @brief    CMSIS compiler specific macros, functions, instructions
- * @version  V1.00
- * @date     06. Jul 2017
+ * @version  V1.0.1
+ * @date     07. Sep 2017
  ******************************************************************************/
 /*
  * Copyright (c) 2009-2017 ARM Limited. All rights reserved.
