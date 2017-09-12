cdd1492bb4915296932a8c3480c52f88ec3306df
Jonatan Antoni <jonatan.antoni@arm.com>
2017-09-12 15:14:07 +0200

    CoreValidation: Added batch build script for IAR examples.

--- CMSIS/Utilities/buildutils/testresult.py
+++ CMSIS/Utilities/buildutils/testresult.py
@@ -24,17 +24,28 @@ class TestResult:
     self._extractXml(log, self._xml)
     self._xml.seek(0)
   
-    report = ElementTree.parse(self._xml).getroot()
-    summary = report[0].findall('summary')[0]
-    self._tests = summary.find('tcnt').text
-    self._executed = summary.find('exec').text
-    self._passed = summary.find('pass').text
-    self._failed = summary.find('fail').text
+    try:
+      report = ElementTree.parse(self._xml).getroot()
+      summary = report[0].findall('summary')[0]
+      self._tests = summary.find('tcnt').text
+      self._executed = summary.find('exec').text
+      self._passed = summary.find('pass').text
+      self._failed = summary.find('fail').text
+      self._avail = True
+    except ElementTree.ParseError as e:
+      self._avail = False
+      self._except = e
           
   def saveXml(self, filename):
     with open(filename, "w") as file:
       self._xml.seek(0)
       shutil.copyfileobj(self._xml, file)
     
+  def isAvailable(self):
+    return self._avail
+    
   def getSummary(self):
-    return self._tests, self._executed, self._passed, self._failed
+    if self._avail:
+      return self._tests, self._executed, self._passed, self._failed
+    else:
+      raise self._except
