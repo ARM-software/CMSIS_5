73879cee933ad0443699f622662224cf7916245f
Jonatan Antoni <jonatan.antoni@arm.com>
2017-09-21 15:01:00 +0200

    CoreValidation: Added validation projects for Cortex-M devices with IAR compiler.

---
+++ CMSIS/CoreValidation/Tests/Cortex-M3/IAR/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * This is an auto generated Run-Time-Environment Component Configuration File
+ * DO NOT MODIFY!
+ *
+ * Project: 'CMSIS_CV'
+ * Device: 'ARMCM3' Pack: 'ARM::CMSIS.5.1.1-dev1'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header "ARMCM3.h"
+
+#endif  /* RTE_COMPONENTS_H */
---
+++ CMSIS/CoreValidation/Tests/Cortex-M7DP/IAR/macros.h
@@ -0,0 +1,300 @@
+#define __CHAR_BITS__ 8
+#define __CHAR_MAX__ 0xff
+#define __CHAR_MIN__ 0
+#define __CHAR_SIZE__ 1
+#define __UNSIGNED_CHAR_MAX__ 0xff
+#define __SIGNED_CHAR_MAX__ 127
+#define __SIGNED_CHAR_MIN__ (-__SIGNED_CHAR_MAX__-1)
+#define __CHAR_ALIGN__ 1
+#define __SHORT_SIZE__ 2
+#define __UNSIGNED_SHORT_MAX__ 0xffff
+#define __SIGNED_SHORT_MAX__ 32767
+#define __SIGNED_SHORT_MIN__ (-__SIGNED_SHORT_MAX__-1)
+#define __SHORT_ALIGN__ 2
+#define __INT_SIZE__ 4
+#define __UNSIGNED_INT_MAX__ 0xffffffffU
+#define __SIGNED_INT_MAX__ 2147483647
+#define __SIGNED_INT_MIN__ (-__SIGNED_INT_MAX__-1)
+#define __INT_ALIGN__ 4
+#define __LONG_SIZE__ 4
+#define __UNSIGNED_LONG_MAX__ 0xffffffffUL
+#define __SIGNED_LONG_MAX__ 2147483647L
+#define __SIGNED_LONG_MIN__ (-__SIGNED_LONG_MAX__-1)
+#define __LONG_ALIGN__ 4
+#define __LONG_LONG_SIZE__ 8
+#define __UNSIGNED_LONG_LONG_MAX__ 0xffffffffffffffffULL
+#define __SIGNED_LONG_LONG_MAX__ 9223372036854775807LL
+#define __SIGNED_LONG_LONG_MIN__ (-__SIGNED_LONG_LONG_MAX__-1)
+#define __LONG_LONG_ALIGN__ 8
+#define __INT8_T_TYPE__ signed char
+#define __INT8_T_MAX__ 127
+#define __INT8_T_MIN__ (-__INT8_T_MAX__-1)
+#define __UINT8_T_TYPE__ unsigned char
+#define __UINT8_T_MAX__ 0xff
+#define __INT8_SIZE_PREFIX__ "hh"
+#define __INT16_T_TYPE__ signed short int
+#define __INT16_T_MAX__ 32767
+#define __INT16_T_MIN__ (-__INT16_T_MAX__-1)
+#define __UINT16_T_TYPE__ unsigned short int
+#define __UINT16_T_MAX__ 0xffff
+#define __INT16_SIZE_PREFIX__ "h"
+#define __INT32_T_TYPE__ signed int
+#define __INT32_T_MAX__ 2147483647
+#define __INT32_T_MIN__ (-__INT32_T_MAX__-1)
+#define __UINT32_T_TYPE__ unsigned int
+#define __UINT32_T_MAX__ 0xffffffffU
+#define __INT32_SIZE_PREFIX__ ""
+#define __INT64_T_TYPE__ signed long long int
+#define __INT64_T_MAX__ 9223372036854775807LL
+#define __INT64_T_MIN__ (-__INT64_T_MAX__-1)
+#define __UINT64_T_TYPE__ unsigned long long int
+#define __UINT64_T_MAX__ 0xffffffffffffffffULL
+#define __INT64_SIZE_PREFIX__ "ll"
+#define __INT_LEAST8_T_TYPE__ signed char
+#define __INT_LEAST8_T_MAX__ 127
+#define __INT_LEAST8_T_MIN__ (-__INT_LEAST8_T_MAX__-1)
+#define __UINT_LEAST8_T_TYPE__ unsigned char
+#define __UINT_LEAST8_T_MAX__ 0xff
+#define __INT8_C_SUFFIX__ 
+#define __UINT8_C_SUFFIX__ 
+#define __INT_LEAST8_SIZE_PREFIX__ "hh"
+#define __INT_LEAST16_T_TYPE__ signed short int
+#define __INT_LEAST16_T_MAX__ 32767
+#define __INT_LEAST16_T_MIN__ (-__INT_LEAST16_T_MAX__-1)
+#define __UINT_LEAST16_T_TYPE__ unsigned short int
+#define __UINT_LEAST16_T_MAX__ 0xffff
+#define __INT16_C_SUFFIX__ 
+#define __UINT16_C_SUFFIX__ 
+#define __INT_LEAST16_SIZE_PREFIX__ "h"
+#define __INT_LEAST32_T_TYPE__ signed int
+#define __INT_LEAST32_T_MAX__ 2147483647
+#define __INT_LEAST32_T_MIN__ (-__INT_LEAST32_T_MAX__-1)
+#define __UINT_LEAST32_T_TYPE__ unsigned int
+#define __UINT_LEAST32_T_MAX__ 0xffffffffU
+#define __INT32_C_SUFFIX__ 
+#define __UINT32_C_SUFFIX__ U
+#define __INT_LEAST32_SIZE_PREFIX__ ""
+#define __INT_LEAST64_T_TYPE__ signed long long int
+#define __INT_LEAST64_T_MAX__ 9223372036854775807LL
+#define __INT_LEAST64_T_MIN__ (-__INT_LEAST64_T_MAX__-1)
+#define __UINT_LEAST64_T_TYPE__ unsigned long long int
+#define __UINT_LEAST64_T_MAX__ 0xffffffffffffffffULL
+#define __INT64_C_SUFFIX__ LL
+#define __UINT64_C_SUFFIX__ ULL
+#define __INT_LEAST64_SIZE_PREFIX__ "ll"
+#define __INT_FAST8_T_TYPE__ signed int
+#define __INT_FAST8_T_MAX__ 2147483647
+#define __INT_FAST8_T_MIN__ (-__INT_FAST8_T_MAX__-1)
+#define __UINT_FAST8_T_TYPE__ unsigned int
+#define __UINT_FAST8_T_MAX__ 0xffffffffU
+#define __INT_FAST8_SIZE_PREFIX__ ""
+#define __INT_FAST16_T_TYPE__ signed int
+#define __INT_FAST16_T_MAX__ 2147483647
+#define __INT_FAST16_T_MIN__ (-__INT_FAST16_T_MAX__-1)
+#define __UINT_FAST16_T_TYPE__ unsigned int
+#define __UINT_FAST16_T_MAX__ 0xffffffffU
+#define __INT_FAST16_SIZE_PREFIX__ ""
+#define __INT_FAST32_T_TYPE__ signed int
+#define __INT_FAST32_T_MAX__ 2147483647
+#define __INT_FAST32_T_MIN__ (-__INT_FAST32_T_MAX__-1)
+#define __UINT_FAST32_T_TYPE__ unsigned int
+#define __UINT_FAST32_T_MAX__ 0xffffffffU
+#define __INT_FAST32_SIZE_PREFIX__ ""
+#define __INT_FAST64_T_TYPE__ signed long long int
+#define __INT_FAST64_T_MAX__ 9223372036854775807LL
+#define __INT_FAST64_T_MIN__ (-__INT_FAST64_T_MAX__-1)
+#define __UINT_FAST64_T_TYPE__ unsigned long long int
+#define __UINT_FAST64_T_MAX__ 0xffffffffffffffffULL
+#define __INT_FAST64_SIZE_PREFIX__ "ll"
+#define __INTMAX_T_TYPE__ signed long long int
+#define __INTMAX_T_MAX__ 9223372036854775807LL
+#define __INTMAX_T_MIN__ (-__INTMAX_T_MAX__-1)
+#define __UINTMAX_T_TYPE__ unsigned long long int
+#define __UINTMAX_T_MAX__ 0xffffffffffffffffULL
+#define __INTMAX_C_SUFFIX__ LL
+#define __UINTMAX_C_SUFFIX__ ULL
+#define __INTMAX_SIZE_PREFIX__ "ll"
+#define __ATOMIC_BOOL_LOCK_FREE 2
+#define __ATOMIC_CHAR_LOCK_FREE 2
+#define __ATOMIC_CHAR16_T_LOCK_FREE 2
+#define __ATOMIC_CHAR32_T_LOCK_FREE 2
+#define __ATOMIC_WCHAR_T_LOCK_FREE 2
+#define __ATOMIC_SHORT_LOCK_FREE 2
+#define __ATOMIC_INT_LOCK_FREE 2
+#define __ATOMIC_LONG_LOCK_FREE 2
+#define __ATOMIC_LLONG_LOCK_FREE 0
+#define __ATOMIC_POINTER_LOCK_FREE 2
+#define __FLOAT_SIZE__ 4
+#define __FLOAT_ALIGN__ 4
+#define __DOUBLE_SIZE__ 8
+#define __DOUBLE_ALIGN__ 8
+#define __LONG_DOUBLE_SIZE__ 8
+#define __LONG_DOUBLE_ALIGN__ 8
+#define __NAN_HAS_HIGH_MANTISSA_BIT_SET__ 0
+#define __SUBNORMAL_FLOATING_POINTS__ 1
+#define __SIZE_T_TYPE__ unsigned int
+#define __SIZE_T_MAX__ 0xffffffffU
+#define __PTRDIFF_T_TYPE__ signed int
+#define __PTRDIFF_T_MAX__ 2147483647
+#define __PTRDIFF_T_MIN__ (-__PTRDIFF_T_MAX__-1)
+#define __INTPTR_T_TYPE__ signed int
+#define __INTPTR_T_MAX__ 2147483647
+#define __INTPTR_T_MIN__ (-__INTPTR_T_MAX__-1)
+#define __UINTPTR_T_TYPE__ unsigned int
+#define __UINTPTR_T_MAX__ 0xffffffffU
+#define __INTPTR_SIZE_PREFIX__ ""
+#define __JMP_BUF_ELEMENT_TYPE__ unsigned long long int
+#define __JMP_BUF_NUM_ELEMENTS__ 16
+#define __TID__ 0xcf60
+#define __VER__ 8011002
+#define __BUILD_NUMBER__ 13589
+#define __IAR_SYSTEMS_ICC__ 9
+#define __LITTLE_ENDIAN__ 1
+#define __BOOL_TYPE__ unsigned char
+#define __BOOL_SIZE__ 1
+#define __WCHAR_T_TYPE__ unsigned long int
+#define __WCHAR_T_SIZE__ 4
+#define __WCHAR_T_MAX__ 0xffffffffU
+#define __DEF_PTR_MEM__ __data
+#define __DEF_PTR_SIZE__ 4
+#define __CODE_MEMORY_LIST1__() __CODE_MEM_HELPER1__(__code, 0)
+#define __CODE_MEMORY_LIST2__(_P1) __CODE_MEM_HELPER2__(__code, 0, _P1)
+#define __CODE_MEMORY_LIST3__(_P1,_P2) __CODE_MEM_HELPER3__(__code, 0, _P1, _P2)
+#define __DATA_MEMORY_LIST1__() __DATA_MEM_HELPER1__(__data, 0)
+#define __DATA_MEMORY_LIST2__(_P1) __DATA_MEM_HELPER2__(__data, 0, _P1)
+#define __DATA_MEMORY_LIST3__(_P1,_P2) __DATA_MEM_HELPER3__(__data, 0, _P1, _P2)
+#define __DATA_MEM0__ __data
+#define __DATA_MEM0_POINTER_OK__ 1
+#define __DATA_MEM0_UNIQUE_POINTER__ 1
+#define __DATA_MEM0_VAR_OK__ 1
+#define __DATA_MEM0_INDEX_TYPE__ int
+#define __DATA_MEM0_SIZE_TYPE__ unsigned int
+#define __DATA_MEM0_INTPTR_TYPE__ int
+#define __DATA_MEM0_UINTPTR_TYPE__ unsigned int
+#define __DATA_MEM0_INTPTR_SIZE_PREFIX__ ""
+#define __DATA_MEM0_MAX_SIZE__ 0x7fffffffU
+#define _RSIZE_MAX 0x7fffffffU
+#define __DATA_MEM0_HEAP_SEGMENT__ "HEAP"
+#define __DATA_MEM0_PAGE_SIZE__ 0
+#define __DATA_MEM0_HEAP__ 0
+#define __CODE_MEM0__ __code
+#define __CODE_MEM0_POINTER_OK__ 1
+#define __CODE_MEM0_UNIQUE_POINTER__ 1
+#define __CODE_PTR_MEMORY_LIST1__() __CODE_PTR_MEM_HELPER1__(__code, 0)
+#define __CODE_PTR_MEMORY_LIST2__(_P1) __CODE_PTR_MEM_HELPER2__(__code, 0, _P1)
+#define __CODE_PTR_MEMORY_LIST3__(_P1,_P2) __CODE_PTR_MEM_HELPER3__(__code, 0, _P1, _P2)
+#define __DATA_PTR_MEMORY_LIST1__() __DATA_PTR_MEM_HELPER1__(__data, 0)
+#define __DATA_PTR_MEMORY_LIST2__(_P1) __DATA_PTR_MEM_HELPER2__(__data, 0, _P1)
+#define __DATA_PTR_MEMORY_LIST3__(_P1,_P2) __DATA_PTR_MEM_HELPER3__(__data, 0, _P1, _P2)
+#define __VAR_MEMORY_LIST1__() __VAR_MEM_HELPER1__(__data, 0)
+#define __VAR_MEMORY_LIST2__(_P1) __VAR_MEM_HELPER2__(__data, 0, _P1)
+#define __VAR_MEMORY_LIST3__(_P1,_P2) __VAR_MEM_HELPER3__(__data, 0, _P1, _P2)
+#define __VARD_MEMORY_LIST1__() __VARD_MEM_HELPER1__(__data, 0, _)
+#define __HEAP_MEM0__ 0
+#define __HEAP_DEFAULT_MEM__ 0
+#define __HEAP_MEMORY_LIST1__() __HEAP_MEM_HELPER1__(__data, 0)
+#define __HEAP_MEMORY_LIST2__(_P1) __HEAP_MEM_HELPER2__(__data, 0, _P1)
+#define __HEAP_MEMORY_LIST3__(_P1,_P2) __HEAP_MEM_HELPER3__(__data, 0, _P1, _P2)
+#define __HVAR_MEMORY_LIST1__() __HVAR_MEM_HELPER1__(__data, 0)
+#define __HEAPD_MEMORY_LIST1__() __HEAPD_MEM_HELPER1__(__data, 0, _)
+#define __HEAPU_MEMORY_LIST1__() __HEAPU_MEM_HELPER1__(__data, 0)
+#define __MULTIPLE_HEAPS__ 0
+#define __TOPM_DATA_MEMORY_LIST1__() 
+#define __TOPM_DATA_MEMORY_LIST2__(_P1) 
+#define __TOPM_DATA_MEMORY_LIST3__(_P1,_P2) 
+#define __TOPP_DATA_MEMORY_LIST1__() __TOPP_DATA_MEM_HELPER1__(__data, 0)
+#define __TOPP_DATA_MEMORY_LIST2__(_P1) __TOPP_DATA_MEM_HELPER2__(__data, 0, _P1)
+#define __TOPP_DATA_MEMORY_LIST3__(_P1,_P2) __TOPP_DATA_MEM_HELPER3__(__data, 0, _P1, _P2)
+#define __DEF_HEAP_MEM__ __data
+#define __PRAGMA_PACK_ON__ 1
+#define __MULTIPLE_INHERITANCE__ 1
+#define __AAPCS_VFP__ 1
+#define __ARM4TM__ 4
+#define __ARM5TM__ 5
+#define __ARM5T__ 5
+#define __ARM5__ 5
+#define __ARM6MEDIA__ 6
+#define __ARM6M__ 11
+#define __ARM6SM__ 12
+#define __ARM6T2__ 6
+#define __ARM6__ 6
+#define __ARM7EM__ 13
+#define __ARM7M__ 7
+#define __ARM7__ 7
+#define __ARMFPV5__ 5
+#define __ARMVFPV1__ 1
+#define __ARMVFPV2__ 2
+#define __ARMVFPV3_D16__ 1
+#define __ARMVFPV3_FP16__ 1
+#define __ARMVFPV3__ 3
+#define __ARMVFPV4__ 4
+#define __ARMVFP_D16__ 1
+#define __ARMVFP_FP16__ 1
+#define __ARMVFP__ __ARMFPV5__
+#define __ARM_ARCH 7
+#define __ARM_ARCH_ISA_THUMB 2
+#define __ARM_ARCH_PROFILE 'M'
+#define __ARM_BIG_ENDIAN 0
+#define __ARM_FEATURE_DSP 1
+#define __ARM_FEATURE_IDIV 1
+#define __ARM_FP 14
+#define __ARM_MEDIA__ 1
+#define __ARM_PROFILE_M__ 1
+#define __CORE__ __ARM7EM__
+#define __CPU_MODE__ 1
+#define __ICCARM__ 1
+#define __INTERWORKING__ 1
+#define __PLAIN_INT_BITFIELD_IS_SIGNED__ 0
+#define __HAS_WEAK__ 1
+#define __HAS_PACKED__ 1
+#define __HAS_JOINED_TYPES__ 1
+#define __HAS_LOCATED_DECLARATION__ 1
+#define __HAS_LOCATED_WITH_INIT__ 1
+#define __IAR_COMPILERBASE__ 655621
+#define __IAR_COMPILERBASE_STR__ 10.1.5.191
+#define __UNICODE_SOURCE_SUPPORTED__ 1
+#define __STDC__ 1
+#define __STDC_VERSION__ 201112L
+#define __STDC_NO_VLA__ 1
+#define __MEMORY_ORDER_RELAXED__ 0
+#define __MEMORY_ORDER_CONSUME__ 1
+#define __MEMORY_ORDER_ACQUIRE__ 2
+#define __MEMORY_ORDER_RELEASE__ 3
+#define __MEMORY_ORDER_ACQ_REL__ 4
+#define __MEMORY_ORDER_SEQ_CST__ 5
+#define __STDC_UTF_16__ 1
+#define __STDC_UTF_32__ 1
+#define __STDC_LIB_EXT1__ 201112L
+#define __STDC_NO_THREADS__ 1
+#define __STDC_ISO_10646__ 201103L
+#define __STDC_HOSTED__ 1
+#define __EDG_IA64_ABI 1
+#define __EDG_IA64_ABI_VARIANT_CTORS_AND_DTORS_RETURN_THIS 1
+#define __EDG_IA64_ABI_USE_INT_STATIC_INIT_GUARD 1
+#define __cpp_unicode_literals 200710
+#define __cpp_static_assert 200410
+#define __EDG_TYPE_TRAITS_ENABLED 1
+#define __EDG__ 1
+#define __EDG_VERSION__ 413
+#define __EDG_SIZE_TYPE__ unsigned int
+#define __EDG_PTRDIFF_TYPE__ int
+#define __EDG_DELTA_TYPE int
+#define __EDG_IA64_VTABLE_ENTRY_TYPE int
+#define __EDG_VIRTUAL_FUNCTION_INDEX_TYPE unsigned short
+#define __EDG_LOWER_VARIABLE_LENGTH_ARRAYS 1
+#define __EDG_IA64_ABI_USE_VARIANT_ARRAY_COOKIES 1
+#define __EDG_ABI_COMPATIBILITY_VERSION 9999
+#define __EDG_ABI_CHANGES_FOR_RTTI 1
+#define __EDG_ABI_CHANGES_FOR_ARRAY_NEW_AND_DELETE 1
+#define __EDG_ABI_CHANGES_FOR_PLACEMENT_DELETE 1
+#define __EDG_BSD 0
+#define __EDG_SYSV 0
+#define __EDG_ANSIC 1
+#define __EDG_CPP11_IL_EXTENSIONS_SUPPORTED 1
+#define __EDG_FLOAT80_ENABLING_POSSIBLE 0
+#define __EDG_FLOAT128_ENABLING_POSSIBLE 0
+#define ARMCM7 1
+#define _RTE_ 1
+#define _DLIB_CONFIG_FILE_HEADER_NAME u8"C:\\tools\\ewarm8\\arm\\INC\\c\\DLib_Config_Full.h"
+#define _DLIB_CONFIG_FILE_STRING u8"C:\\tools\\ewarm8\\arm\\INC\\c\\DLib_Config_Full.h"
+#define __VERS
