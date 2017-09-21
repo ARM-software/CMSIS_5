18ba7199851b8881e0ec9094d8dcc600243c0694
Jonatan Antoni <jonatan.antoni@arm.com>
2017-09-21 15:01:00 +0200

    CoreValidation: Added validation projects for Cortex-M devices with IAR compiler.

---
+++ CMSIS/CoreValidation/Tests/Cortex-M4FP/IAR/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * This is an auto generated Run-Time-Environment Component Configuration File
+ * DO NOT MODIFY!
+ *
+ * Project: 'CMSIS_CV'
+ * Device: 'ARMCM4_FP' Pack: 'ARM::CMSIS.5.1.1-dev1'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header "ARMCM4_FP.h"
+
+#endif  /* RTE_COMPONENTS_H */
---
+++ CMSIS/CoreValidation/Tests/Cortex-M7SP/IAR/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * This is an auto generated Run-Time-Environment Component Configuration File
+ * DO NOT MODIFY!
+ *
+ * Project: 'CMSIS_CV'
+ * Device: 'ARMCM7_SP' Pack: 'ARM::CMSIS.5.1.1-dev1'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header "ARMCM7_SP.h"
+
+#endif  /* RTE_COMPONENTS_H */
