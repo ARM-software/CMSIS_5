1aead4c5e2bbc0673e0843824486d9feb85dd136
Jonatan Antoni <jonatan.antoni@arm.com>
2017-09-25 16:42:40 +0200

    CoreValidation: Added test projects for ARMv8-M running tests in non-secure.

---
+++ CMSIS/CoreValidation/Tests/Cortex-M23NS/IAR/Bootloader/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * This is an auto generated Run-Time-Environment Component Configuration File
+ * DO NOT MODIFY!
+ *
+ * Project: 'Bootloader'
+ * Device: 'ARMCM23_TZ' Pack: 'ARM::CMSIS.5.1.2-dev2'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header "ARMCM23_TZ.h"
+
+#endif  /* RTE_COMPONENTS_H */
