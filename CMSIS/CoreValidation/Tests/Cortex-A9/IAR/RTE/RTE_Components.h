f7d64b104ef2b40fc99e1b08790108e2fcbeb1b7
Jonatan Antoni <jonatan.antoni@arm.com>
2017-10-10 16:17:25 +0200

    CoreValidation: Added test project for Cortex-A9 using IAR tools.

---
+++ CMSIS/CoreValidation/Tests/Cortex-A9/IAR/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * This is an auto generated Run-Time-Environment Component Configuration File
+ * DO NOT MODIFY!
+ *
+ * Project: 'CMSIS_CV'
+ * Device: 'ARMCA9' Pack: 'ARM::CMSIS.5.1.2-dev2'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header "ARMCA9.h"
+
+#endif  /* RTE_COMPONENTS_H */
