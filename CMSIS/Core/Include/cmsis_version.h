334b5dce437bf81d6c2d578b40cfbfe32d28da79
Jonatan Antoni <jonatan.antoni@arm.com>
2017-11-10 11:40:06 +0100

    Release preparation: Aligned version information and updated change histories.

--- CMSIS/Core/Include/cmsis_version.h
+++ CMSIS/Core/Include/cmsis_version.h
@@ -33,7 +33,7 @@
 
 /*  CMSIS Version definitions */
 #define __CM_CMSIS_VERSION_MAIN  ( 5U)                                      /*!< [31:16] CMSIS Core(M) main version */
-#define __CM_CMSIS_VERSION_SUB   ( 0U)                                      /*!< [15:0]  CMSIS Core(M) sub version */
+#define __CM_CMSIS_VERSION_SUB   ( 1U)                                      /*!< [15:0]  CMSIS Core(M) sub version */
 #define __CM_CMSIS_VERSION       ((__CM_CMSIS_VERSION_MAIN << 16U) | \
                                    __CM_CMSIS_VERSION_SUB           )       /*!< CMSIS Core(M) version number */
 #endif
