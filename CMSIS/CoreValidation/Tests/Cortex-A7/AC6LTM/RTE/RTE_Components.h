85f3ef85f0d612dbed58f0f584eaf0a5f3aff6e2
Jonatan Antoni <jonatan.antoni@arm.com>
2017-11-16 10:16:55 +0100

    CoreValidation: Added test projects for ArmCompiler6 LTM on Cortex-A.

---
+++ CMSIS/CoreValidation/Tests/Cortex-A7/AC6LTM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: CMSIS_CV_CA7_AC6
+ * RTE configuration: CMSIS_CV_CA7_AC6.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "ARMCA7.h"
+
+#define RTE_CV_COREFUNC                       /* CORE Validation - Core Function tests enabled */
+#define RTE_CV_COREINSTR                      /* CORE Validation - Core Instruction tests enabled */
+#define RTE_CV_GENTIMER                       /* CORE Validation - Generic Timer tests enabled */
+
+#endif /* RTE_COMPONENTS_H */
