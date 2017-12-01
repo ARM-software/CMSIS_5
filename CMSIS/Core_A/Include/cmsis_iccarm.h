1f314931b80fdf1019c039ee22ce4915626a9291
TomoYamanaka <tomoyuki.yamanaka.xd@renesas.com>
2017-12-02 00:56:46 +0900

    CMSIS-Core(A): Changed __L1C_CleanInvalidateCache function for IAR.
    I changed function by basing on cmsis_gcc.h because the build error(assembly syntax error) will occurs.

--- CMSIS/Core_A/Include/cmsis_iccarm.h
+++ CMSIS/Core_A/Include/cmsis_iccarm.h
@@ -480,62 +480,77 @@ __IAR_FT void __set_SP_usr(uint32_t topOfProcStack)
 #define __get_mode()                (__get_CPSR() & 0x1FU)
 
 
+__STATIC_FORCEINLINE int32_t log2_up(uint32_t n)
+{
+  int32_t log = -1;
+  uint32_t t = n;
+  while(t)
+  {
+    log++; t >>=1;
+  }
+  /* if n not power of 2 -> round up*/
+  if ( n & (n - 1) ) log++;
+  return log;
+}
+
+__STATIC_INLINE void __L1C_MaintainDCacheSetWay(uint32_t level, uint32_t maint)
+{
+  register volatile uint32_t Dummy;
+  register volatile uint32_t ccsidr;
+  uint32_t num_sets;
+  uint32_t num_ways;
+  uint32_t shift_way;
+  uint32_t log2_linesize;
+  uint32_t log2_num_ways;
+
+  Dummy = level << 1;
+  /* set csselr, select ccsidr register */
+  __set_CCSIDR(Dummy);
+  /* get current ccsidr register */
+  ccsidr = __get_CCSIDR();
+  num_sets = ((ccsidr & 0x0FFFE000) >> 13) + 1;
+  num_ways = ((ccsidr & 0x00001FF8) >> 3) + 1;
+  log2_linesize = (ccsidr & 0x00000007) + 2 + 2;
+  log2_num_ways = log2_up(num_ways);
+  shift_way = 32 - log2_num_ways;
+  for(int way = num_ways-1; way >= 0; way--) {
+    for(int set = num_sets-1; set >= 0; set--) {
+      Dummy = (level << 1) | (set << log2_linesize) | (way << shift_way);
+      switch (maint)
+      {
+        case 0:
+          __ASM volatile("MCR p15, 0, %0, c7, c6, 2" : : "r"(Dummy) : "memory"); // DCISW. Invalidate by Set/Way
+          break;
+
+        case 1:
+          __ASM volatile("MCR p15, 0, %0, c7, c10, 2" : : "r"(Dummy) : "memory"); // DCCSW. Clean by Set/Way
+          break;
+
+        default:
+          __ASM volatile("MCR p15, 0, %0, c7, c14, 2" : : "r"(Dummy) : "memory"); // DCCISW. Clean and Invalidate by Set/Way
+          break;
+
+      }
+    }
+  }
+  __DMB();
+}
+
 __STATIC_INLINE
 void __L1C_CleanInvalidateCache(uint32_t op)
 {
-  __ASM volatile(
-    "        PUSH    {R4-R11}                   \n"
-
-    "        MRC     p15, 1, R6, c0, c0, 1      \n" // Read CLIDR
-    "        ANDS    R3, R6, #0x07000000        \n" // Extract coherency level
-    "        MOV     R3, R3, LSR #23            \n" // Total cache levels << 1
-    "        BEQ     Finished                   \n" // If 0, no need to clean
-
-    "        MOV     R10, #0                    \n" // R10 holds current cache level << 1
-    "Loop1:  ADD     R2, R10, R10, LSR #1       \n" // R2 holds cache "Set" position
-    "        MOV     R1, R6, LSR R2             \n" // Bottom 3 bits are the Cache-type for this level
-    "        AND     R1, R1, #7                 \n" // Isolate those lower 3 bits
-    "        CMP     R1, #2                     \n"
-    "        BLT     Skip                       \n" // No cache or only instruction cache at this level
-
-    "        MCR     p15, 2, R10, c0, c0, 0     \n" // Write the Cache Size selection register
-    "        ISB                                \n" // ISB to sync the change to the CacheSizeID reg
-    "        MRC     p15, 1, R1, c0, c0, 0      \n" // Reads current Cache Size ID register
-    "        AND     R2, R1, #7                 \n" // Extract the line length field
-    "        ADD     R2, R2, #4                 \n" // Add 4 for the line length offset (log2 16 bytes)
-    "        MOVW    R4, #0x3FF                 \n"
-    "        ANDS    R4, R4, R1, LSR #3         \n" // R4 is the max number on the way size (right aligned)
-    "        CLZ     R5, R4                     \n" // R5 is the bit position of the way size increment
-    "        MOVW    R7, #0x7FFF                \n"
-    "        ANDS    R7, R7, R1, LSR #13        \n" // R7 is the max number of the index size (right aligned)
-
-    "Loop2:  MOV     R9, R4                     \n" // R9 working copy of the max way size (right aligned)
-
-    "Loop3:  ORR     R11, R10, R9, LSL R5       \n" // Factor in the Way number and cache number into R11
-    "        ORR     R11, R11, R7, LSL R2       \n" // Factor in the Set number
-    "        CMP     R0, #0                     \n"
-    "        BNE     Dccsw                      \n"
-    "        MCR     p15, 0, R11, c7, c6, 2     \n" // DCISW. Invalidate by Set/Way
-    "        B       cont                       \n"
-    "Dccsw:  CMP     R0, #1                     \n"
-    "        BNE     Dccisw                     \n"
-    "        MCR     p15, 0, R11, c7, c10, 2    \n" // DCCSW. Clean by Set/Way
-    "        B       cont                       \n"
-    "Dccisw: MCR     p15, 0, R11, c7, c14, 2    \n" // DCCISW. Clean and Invalidate by Set/Way
-    "cont:   SUBS    R9, R9, #1                 \n" // Decrement the Way number
-    "        BGE     Loop3                      \n"
-    "        SUBS    R7, R7, #1                 \n" // Decrement the Set number
-    "        BGE     Loop2                      \n"
-    "Skip:   ADD     R10, R10, #2               \n" // Increment the cache number
-    "        CMP     R3, R10                    \n"
-    "        BGT     Loop1                      \n"
-
-    "Finished:                                  \n"
-    "        DSB                                \n"
-    "        POP    {R4-R11}                      "
-  );
+  register volatile uint32_t clidr;
+  uint32_t cache_type;
+  clidr =  __get_CLIDR();
+  for(uint32_t i = 0; i<7; i++)
+  {
+    cache_type = (clidr >> i*3) & 0x7UL;
+    if ((cache_type >= 2) && (cache_type <= 4))
+    {
+      __L1C_MaintainDCacheSetWay(i, op);
+    }
+  }
 }
-
 
 __STATIC_INLINE
 void __FPU_Enable(void)
