112c69959d4089dd396647fbe364e2265c2222c2
Christopher Seidl <christopher.seidl@arm.com>
2018-01-09 10:25:55 +0100

    Re-added flash programming algorithm example, now in MDK v5.

---
+++ Device/_Template_Flash/FlashPrg.c
@@ -0,0 +1,128 @@
+/* Copyright (c) 2010 - 2018 Arm Ltd.
+
+   All rights reserved.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+   - Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+   - Neither the name of Arm nor the names of its contributors may be used
+     to endorse or promote products derived from this software without
+     specific prior written permission.
+   *
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+   ---------------------------------------------------------------------------*/
+   
+/***********************************************************************/
+/*                                                                     */
+/*  FlashPrg.c: Flash Programming Functions adapted                    */
+/*              for New Device Flash                                   */
+/*                                                                     */
+/***********************************************************************/
+
+#include "FlashOS.h"        // FlashOS Structures
+
+/* 
+   Mandatory Flash Programming Functions (Called by FlashOS):
+                int Init        (unsigned long adr,   // Initialize Flash
+                                 unsigned long clk,
+                                 unsigned long fnc);
+                int UnInit      (unsigned long fnc);  // De-initialize Flash
+                int EraseSector (unsigned long adr);  // Erase Sector Function
+                int ProgramPage (unsigned long adr,   // Program Page Function
+                                 unsigned long sz,
+                                 unsigned char *buf);
+
+   Optional  Flash Programming Functions (Called by FlashOS):
+                int BlankCheck  (unsigned long adr,   // Blank Check
+                                 unsigned long sz,
+                                 unsigned char pat);
+                int EraseChip   (void);               // Erase complete Device
+      unsigned long Verify      (unsigned long adr,   // Verify Function
+                                 unsigned long sz,
+                                 unsigned char *buf);
+
+       - BlanckCheck  is necessary if Flash space is not mapped into CPU memory space
+       - Verify       is necessary if Flash space is not mapped into CPU memory space
+       - if EraseChip is not provided than EraseSector for all sectors is called
+*/
+
+
+/*
+ *  Initialize Flash Programming Functions
+ *    Parameter:      adr:  Device Base Address
+ *                    clk:  Clock Frequency (Hz)
+ *                    fnc:  Function Code (1 - Erase, 2 - Program, 3 - Verify)
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+int Init (unsigned long adr, unsigned long clk, unsigned long fnc) {
+
+  /* Add your Code */
+  return (0);                                  // Finished without Errors
+}
+
+
+/*
+ *  De-Initialize Flash Programming Functions
+ *    Parameter:      fnc:  Function Code (1 - Erase, 2 - Program, 3 - Verify)
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+int UnInit (unsigned long fnc) {
+
+  /* Add your Code */
+  return (0);                                  // Finished without Errors
+}
+
+
+/*
+ *  Erase complete Flash Memory
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+int EraseChip (void) {
+
+  /* Add your Code */
+  return (0);                                  // Finished without Errors
+}
+
+
+/*
+ *  Erase Sector in Flash Memory
+ *    Parameter:      adr:  Sector Address
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+int EraseSector (unsigned long adr) {
+
+  /* Add your Code */
+  return (0);                                  // Finished without Errors
+}
+
+
+/*
+ *  Program Page in Flash Memory
+ *    Parameter:      adr:  Page Start Address
+ *                    sz:   Page Size
+ *                    buf:  Page Data
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+int ProgramPage (unsigned long adr, unsigned long sz, unsigned char *buf) {
+
+  /* Add your Code */
+  return (0);                                  // Finished without Errors
+}
