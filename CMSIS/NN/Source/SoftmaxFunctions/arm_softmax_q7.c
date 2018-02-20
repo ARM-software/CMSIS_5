4e515bb60f8b4daa314163e9a6bd07499b653c87
Liangzhen Lai <liangzhen.lai@arm.com>
2018-02-20 11:29:57 -0800

    Complete the update and testing of softmax functions

--- CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c
+++ CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c
@@ -21,7 +21,7 @@
  * Title:        arm_softmax_q7.c
  * Description:  Q7 softmax function
  *
- * $Date:        17. January 2018
+ * $Date:        20. February 2018
  * $Revision:    V.1.0.0
  *
  * Target Processor:  Cortex-M cores
@@ -77,7 +77,8 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out)
         }
     }
 
-    /* So the base is set to max-8, meaning 
+    /* 
+     * So the base is set to max-8, meaning 
      * that we ignore really small values. 
      * anyway, they will be 0 after shrinking to q7_t.
      */
@@ -87,23 +88,27 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out)
 
     for (i = 0; i < dim_vec; i++)
     {
-        if (vec_in[i] > base) {
-          shift = (uint8_t)__USAT(vec_in[i] - base, 7);
-          sum += 0x1 << shift;
+        if (vec_in[i] > base) 
+        {
+            shift = (uint8_t)__USAT(vec_in[i] - base, 5);
+            sum += 0x1 << shift;
         }
     }
 
     /* This is effectively (0x1 << 20) / sum */
     int output_base = 0x100000 / sum;
 
-    /* Final confidence will be output_base >> ( 13 - (vec_in[i] - min) )
-     * so 128 (0x1<<7) -> 100% confidence when output_base = 0x1<<12 and vec_in[i]-min = 8
+    /* 
+     * Final confidence will be output_base >> ( 13 - (vec_in[i] - base) )
+     * so 128 (0x1<<7) -> 100% confidence when sum = 0x1 << 8, output_base = 0x1 << 12 
+     * and vec_in[i]-base = 8
      */
     for (i = 0; i < dim_vec; i++) 
     {
-        if (vec_in[i] > base) {
-            /* Here minimum value of 13+min-vec_in[i] will be 5 */
-            shift = (uint8_t)__USAT(13+base-vec_in[i], 7);
+        if (vec_in[i] > base) 
+        {
+            /* Here minimum value of 13+base-vec_in[i] will be 5 */
+            shift = (uint8_t)__USAT(13+base-vec_in[i], 5);
             p_out[i] = (q7_t) __SSAT((output_base >> shift), 8);
         } else {
             p_out[i] = 0;
