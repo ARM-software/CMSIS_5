017de4145a1672f9625eaa3874aae6b1cd63c02c
Robert Rostohar <Robert.Rostohar@arm.com>
2018-05-23 11:19:22 +0200

    RTX5: removed compiler warning when Timers are not used (#316)

--- CMSIS/RTOS2/RTX/Source/rtx_lib.c
+++ CMSIS/RTOS2/RTX/Source/rtx_lib.c
@@ -225,7 +225,7 @@ static const osMessageQueueAttr_t os_timer_mq_attr = {
 #else
 
 extern void osRtxTimerThread (void *argument);
-       void osRtxTimerThread (void *argument) {}
+       void osRtxTimerThread (void *argument) { (void)argument; }
 
 #endif  // ((OS_TIMER_THREAD_STACK_SIZE != 0) && (OS_TIMER_CB_QUEUE != 0))
 
