a540b2a37ac3aa63c42f71130cda48f8d5537d29
Daniel Brondani <daniel.brondani@arm.com>
2018-11-23 16:43:43 +0100

    Cortex-A: Corrected Inner Cacheability attributes for Translation Table TTBR0 in MMU config. SDMDK-8133

--- Device/_Template_Vendor/Vendor/Device_A/Source/mmu_Device.c
+++ Device/_Template_Vendor/Vendor/Device_A/Source/mmu_Device.c
@@ -215,13 +215,13 @@ void MMU_CreateTranslationTable(void)
   /* Set location of level 1 page table
   ; 31:14 - Translation table base addr (31:14-TTBCR.N, TTBCR.N is 0 out of reset)
   ; 13:7  - 0x0
-  ; 6     - IRGN[0] 0x0 (Inner WB WA)
-  ; 5     - NOS     0x0 (Non-shared)
-  ; 4:3   - RGN     0x1 (Outer WB WA)
-  ; 2     - IMP     0x0 (Implementation Defined)
-  ; 1     - S       0x0 (Non-shared)
-  ; 0     - IRGN[1] 0x1 (Inner WB WA) */
-  __set_TTBR0(((uint32_t)&Image$$TTB$$ZI$$Base) | 9U);
+  ; 6     - IRGN[0] 0x1  (Inner WB WA)
+  ; 5     - NOS     0x0  (Non-shared)
+  ; 4:3   - RGN     0x01 (Outer WB WA)
+  ; 2     - IMP     0x0  (Implementation Defined)
+  ; 1     - S       0x0  (Non-shared)
+  ; 0     - IRGN[1] 0x0  (Inner WB WA) */
+  __set_TTBR0(((uint32_t)&Image$$TTB$$ZI$$Base) | 0x48);
   __ISB();
 
   /* Set up domain access control register
