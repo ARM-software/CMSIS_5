e214c71df646aff289ceb06f540a029ead096666
Jonatan Antoni <jonatan.antoni@arm.com>
2019-02-19 17:25:34 +0100

    CoreValidation: Added test configurations for Cortex-M35P.
    Change-Id: Iadf4f650e478470847272c16a9bc5714122286c8

--- CMSIS/CoreValidation/Tests/bootloader/RTE_Components.h
+++ CMSIS/CoreValidation/Tests/bootloader/RTE_Components.h
@@ -5,6 +5,8 @@
 #define CMSIS_device_header "ARMCM23_TZ.h"
 #elif defined(ARMCM33_DSP_FP_TZ)
 #define CMSIS_device_header "ARMCM33_DSP_FP_TZ.h"
+#elif defined(ARMCM35P_DSP_FP_TZ)
+#define CMSIS_device_header "ARMCM35P_DSP_FP_TZ.h"
 #else
 #error "Unknown device selection!"
 #endif
