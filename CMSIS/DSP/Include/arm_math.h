1410acec6aa059a3602592808d04cd1363ee96c1
GuentherMartin <martin.guenther@arm.com>
2019-03-18 08:13:10 +0100

    Added SIMD macros to arm_math.h again. CMSIS NN uses this macros.

--- CMSIS/DSP/Include/arm_math.h
+++ CMSIS/DSP/Include/arm_math.h
@@ -400,6 +400,34 @@ extern "C"
   typedef double float64_t;
 
 
+/**
+  @brief definition to read/write two 16 bit values (Depricated).
+ */
+#if   defined ( __CC_ARM )
+  #define __SIMD32_TYPE int32_t __packed
+#elif defined ( __ARMCC_VERSION ) && ( __ARMCC_VERSION >= 6010050 )
+  #define __SIMD32_TYPE int32_t
+#elif defined ( __GNUC__ )
+  #define __SIMD32_TYPE int32_t
+#elif defined ( __ICCARM__ )
+  #define __SIMD32_TYPE int32_t __packed
+#elif defined ( __TI_ARM__ )
+  #define __SIMD32_TYPE int32_t
+#elif defined ( __CSMC__ )
+  #define __SIMD32_TYPE int32_t
+#elif defined ( __TASKING__ )
+  #define __SIMD32_TYPE __unaligned int32_t
+#else
+  #error Unknown compiler
+#endif
+
+#define __SIMD32(addr)        (*(__SIMD32_TYPE **) & (addr))
+#define __SIMD32_CONST(addr)  ( (__SIMD32_TYPE * )   (addr))
+#define _SIMD32_OFFSET(addr)  (*(__SIMD32_TYPE * )   (addr))
+#define __SIMD64(addr)        (*(      int64_t **) & (addr))
+
+
+
 
 /* SIMD replacement */
 
