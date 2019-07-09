89137506efdc2d7445225efd7be008f242184e4a
Rajmund Szymański <rajszym@users.noreply.github.com>
2019-07-09 09:48:37 +0200

    extern keyword not required
    the extern keyword is not required for the external function declaration

--- CMSIS/Core/Include/cmsis_gcc.h
+++ CMSIS/Core/Include/cmsis_gcc.h
@@ -130,7 +130,7 @@
  */
 __STATIC_FORCEINLINE __NO_RETURN void __cmsis_start(void)
 {
-  extern void _start(void) __NO_RETURN;
+  void _start(void) __NO_RETURN;
   
   typedef struct {
     uint32_t const* src;
