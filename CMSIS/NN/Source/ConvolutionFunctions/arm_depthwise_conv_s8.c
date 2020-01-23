69ebf239e621d63e834a3e17b0ad810c4064bab3
Giorgio Arena <giorgio.arena@arm.com>
2020-01-23 11:15:49 +0000

    CMSIS-NN Opt: Optimize Depthwise convolution on Cortex-M4/M7 (non-DSP)

--- CMSIS/NN/Source/ConvolutionFunctions/arm_depthwise_conv_s8.c
+++ CMSIS/NN/Source/ConvolutionFunctions/arm_depthwise_conv_s8.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2019 Arm Limited or its affiliates. All rights reserved.
+ * Copyright (C) 2010-2020 Arm Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -21,8 +21,8 @@
  * Title:        arm_depthwise_conv_s8.c
  * Description:	 s8 version of depthwise convolution.
  *
- * $Date:        July 2019
- * $Revision:    V.0.5.0
+ * $Date:        January 2019
+ * $Revision:    V.1.0.0
  *
  * Target Processor:  Cortex-M cores
  *
@@ -40,46 +40,109 @@
  * @{
  */
 
-  /*
-   *  Basic s8 depthwise convolution function.
-   *
-   *  Refer header file for details.
-   *  Optimization using DSP extension is not available for the generic case where channel multiplier is > 1.
-   *
-   */
-arm_status arm_depthwise_conv_s8(const q7_t *input,
-                                 const uint16_t input_x,
-                                 const uint16_t input_y,
-                                 const uint16_t input_ch,
-                                 const q7_t *kernel,
-                                 const uint16_t output_ch,
-                                 const uint16_t ch_mult,
-                                 const uint16_t kernel_x,
-                                 const uint16_t kernel_y,
-                                 const uint16_t pad_x,
-                                 const uint16_t pad_y,
-                                 const uint16_t stride_x,
-                                 const uint16_t stride_y,
-                                 const int32_t *bias,
-                                 q7_t *output,
-                                 const int32_t *output_shift,
-                                 const int32_t *output_mult,
-                                 const uint16_t output_x,
-                                 const uint16_t output_y,
-                                 const int32_t output_offset,
-                                 const int32_t input_offset,
-                                 const int32_t output_activation_min,
-                                 const int32_t output_activation_max,
-                                 const uint16_t dilation_x,
-                                 const uint16_t dilation_y,
-                                 q15_t *buffer_a)
+static void depthwise_conv_s8_mult_4(const int8_t *input,
+                                     const int32_t input_x,
+                                     const int32_t input_y,
+                                     const int32_t input_ch,
+                                     const int8_t *kernel,
+                                     const int32_t output_ch,
+                                     const int32_t ch_mult,
+                                     const int32_t kernel_x,
+                                     const int32_t kernel_y,
+                                     const int32_t pad_x,
+                                     const int32_t pad_y,
+                                     const int32_t stride_x,
+                                     const int32_t stride_y,
+                                     const int32_t *bias,
+                                     int8_t *output,
+                                     const int32_t *output_shift,
+                                     const int32_t *output_mult,
+                                     const int32_t output_x,
+                                     const int32_t output_y,
+                                     const int32_t output_offset,
+                                     const int32_t input_offset,
+                                     const int32_t output_activation_min,
+                                     const int32_t output_activation_max)
+{
+    for(int32_t in_h = -pad_y, out_h = 0, out_idx = 0; out_h < output_y; in_h += stride_y, ++out_h)
+    {
+        for(int32_t in_w = -pad_x, out_w = 0, ker_h_start = MAX(0, -in_h); out_w < output_x; in_w += stride_x, ++out_w)
+        {
+            for(int32_t in_ch = 0, out_ch = 0, ker_w_start = MAX(0, -in_w); out_ch < output_ch; ++in_ch, out_ch += ch_mult)
+            {
+                for(int mult_tile = 0; mult_tile < ch_mult; mult_tile += 4)
+                {
+                    int32_t out_buff0 = bias[out_ch + 0 + mult_tile];
+                    int32_t out_buff1 = bias[out_ch + 1 + mult_tile];
+                    int32_t out_buff2 = bias[out_ch + 2 + mult_tile];
+                    int32_t out_buff3 = bias[out_ch + 3 + mult_tile];
+
+                    for(int32_t ker_h = ker_h_start; ker_h < MIN(kernel_y, input_y - in_h); ++ker_h)
+                    {
+                        int32_t ker_idx = ker_h * (output_ch * kernel_x) + ker_w_start * output_ch + out_ch;
+                        int32_t in_idx  = (in_h + ker_h) * (input_ch * input_x) + in_w * input_ch + in_ch;
+
+                        for(int32_t ker_w = ker_w_start; ker_w < MIN(kernel_x, input_x - in_w); ++ker_w, ker_idx += output_ch)
+                        {
+                            int32_t in_val = input[in_idx + ker_w * input_ch] + input_offset;
+
+                            out_buff0 += in_val * kernel[ker_idx + 0 + mult_tile];
+                            out_buff1 += in_val * kernel[ker_idx + 1 + mult_tile];
+                            out_buff2 += in_val * kernel[ker_idx + 2 + mult_tile];
+                            out_buff3 += in_val * kernel[ker_idx + 3 + mult_tile];
+                        }
+                    }
+
+                    out_buff0 = arm_nn_requantize(out_buff0, output_mult[out_ch + 0 + mult_tile], output_shift[out_ch + 0 + mult_tile]);
+                    out_buff1 = arm_nn_requantize(out_buff1, output_mult[out_ch + 1 + mult_tile], output_shift[out_ch + 1 + mult_tile]);
+                    out_buff2 = arm_nn_requantize(out_buff2, output_mult[out_ch + 2 + mult_tile], output_shift[out_ch + 2 + mult_tile]);
+                    out_buff3 = arm_nn_requantize(out_buff3, output_mult[out_ch + 3 + mult_tile], output_shift[out_ch + 3 + mult_tile]);
+
+                    out_buff0 += output_offset;
+                    out_buff1 += output_offset;
+                    out_buff2 += output_offset;
+                    out_buff3 += output_offset;
+
+                    out_buff0 = MIN(MAX(out_buff0, output_activation_min), output_activation_max);
+                    out_buff1 = MIN(MAX(out_buff1, output_activation_min), output_activation_max);
+                    out_buff2 = MIN(MAX(out_buff2, output_activation_min), output_activation_max);
+                    out_buff3 = MIN(MAX(out_buff3, output_activation_min), output_activation_max);
+
+                    output[out_idx++] = (int8_t)out_buff0;
+                    output[out_idx++] = (int8_t)out_buff1;
+                    output[out_idx++] = (int8_t)out_buff2;
+                    output[out_idx++] = (int8_t)out_buff3;
+                }
+            }
+        }
+    }
+}
+
+static void depthwise_conv_s8_generic(const q7_t *input,
+                                      const uint16_t input_x,
+                                      const uint16_t input_y,
+                                      const uint16_t input_ch,
+                                      const q7_t *kernel,
+                                      const uint16_t output_ch,
+                                      const uint16_t ch_mult,
+                                      const uint16_t kernel_x,
+                                      const uint16_t kernel_y,
+                                      const uint16_t pad_x,
+                                      const uint16_t pad_y,
+                                      const uint16_t stride_x,
+                                      const uint16_t stride_y,
+                                      const int32_t *bias,
+                                      q7_t *output,
+                                      const int32_t *output_shift,
+                                      const int32_t *output_mult,
+                                      const uint16_t output_x,
+                                      const uint16_t output_y,
+                                      const int32_t output_offset,
+                                      const int32_t input_offset,
+                                      const int32_t output_activation_min,
+                                      const int32_t output_activation_max)
 {
     int i_out = 0;
-    (void)dilation_x;
-    (void)dilation_y;
-    (void)buffer_a;
-    (void)output_ch;
-
     for (int i_out_y = 0; i_out_y < output_y; i_out_y++)
     {
         const int16_t base_idx_y = (i_out_y * stride_y) - pad_y;
@@ -123,6 +186,56 @@ arm_status arm_depthwise_conv_s8(const q7_t *input,
                 }
             }
         }
+    }
+}
+
+  /*
+   *  Basic s8 depthwise convolution function.
+   *
+   *  Refer header file for details.
+   *  Optimization using DSP extension is not available for the generic case where channel multiplier is > 1.
+   *
+   */
+arm_status arm_depthwise_conv_s8(const q7_t *input,
+                                        const uint16_t input_x,
+                                        const uint16_t input_y,
+                                        const uint16_t input_ch,
+                                        const q7_t *kernel,
+                                        const uint16_t output_ch,
+                                        const uint16_t ch_mult,
+                                        const uint16_t kernel_x,
+                                        const uint16_t kernel_y,
+                                        const uint16_t pad_x,
+                                        const uint16_t pad_y,
+                                        const uint16_t stride_x,
+                                        const uint16_t stride_y,
+                                        const int32_t *bias,
+                                        q7_t *output,
+                                        const int32_t *output_shift,
+                                        const int32_t *output_mult,
+                                        const uint16_t output_x,
+                                        const uint16_t output_y,
+                                        const int32_t output_offset,
+                                        const int32_t input_offset,
+                                        const int32_t output_activation_min,
+                                        const int32_t output_activation_max,
+                                        const uint16_t dilation_x,
+                                        const uint16_t dilation_y,
+                                        q15_t *buffer_a)
+{
+    (void)dilation_x;
+    (void)dilation_y;
+    (void)buffer_a;
+
+    if(ch_mult % 4 == 0)
+    {
+        depthwise_conv_s8_mult_4(input, input_x, input_y, input_ch, kernel, output_ch, ch_mult, kernel_x, kernel_y, pad_x, pad_y, stride_x, stride_y, bias,
+                                 output, output_shift, output_mult, output_x, output_y, output_offset, input_offset, output_activation_min, output_activation_max);
+    }
+    else
+    {
+        depthwise_conv_s8_generic(input, input_x, input_y, input_ch, kernel, output_ch, ch_mult, kernel_x, kernel_y, pad_x, pad_y, stride_x, stride_y, bias,
+                                  output, output_shift, output_mult, output_x, output_y, output_offset, input_offset, output_activation_min, output_activation_max);
     }
 
     /* Return to application */
