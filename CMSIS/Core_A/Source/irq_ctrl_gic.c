7e7e61222d4f5364c5c29a1b01d3006a5b8020ab
ua1arn <mgs2001@mail.ru>
2020-03-03 12:01:05 +0300

    Core-A: modifications for representation interrupt mode model on Renesas Cortex-A9 RZA1x CPUs

--- CMSIS/Core_A/Source/irq_ctrl_gic.c
+++ CMSIS/Core_A/Source/irq_ctrl_gic.c
@@ -148,6 +148,11 @@ __WEAK int32_t IRQ_SetMode (IRQn_ID_t irqn, uint32_t mode) {
       status = -1;
     }
 
+    val = (mode & IRQ_MODE_MODEL_Msk);
+    if (val == IRQ_MODE_MODEL_1N) {
+    	 cfg |= 1;   // 1-N model
+    }
+
     // Check interrupt type
     val = mode & IRQ_MODE_TYPE_Msk;
 
@@ -216,6 +221,9 @@ __WEAK uint32_t IRQ_GetMode (IRQn_ID_t irqn) {
       mode |= IRQ_MODE_TRIG_LEVEL;
     }
 
+    if (val & 1U) {
+    	 mode |= IRQ_MODE_MODEL_1N;
+    }
     // Get interrupt CPU targets
     mode |= GIC_GetTarget ((IRQn_Type)irqn) << IRQ_MODE_CPU_Pos;
 
