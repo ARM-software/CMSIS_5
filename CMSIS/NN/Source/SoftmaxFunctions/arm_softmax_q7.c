ARM-software/CMSIS_5

commit ede5bd3e2c8290d118f1f1f74ad99603da43730d

CMSIS-NN: Optimized version of original arm_softmax_q7 function.

CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c
@@ -60,6 +60,8 @@
    *
    */
 
+#if defined(ARM_MATH_LOOPUNROLL) && defined (ARM_MATH_DSP)
+
 void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
 {
     q31_t     sum;
@@ -67,8 +69,19 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
     uint8_t   shift;
     q15_t     base;
     base = -128;
+    uint16_t blkCnt;
+
+    q31_t in,in1,in2;
+    q31_t out1, out2;
+
+    q31_t baseV;
+    q31_t shiftV;
+    const q31_t pad=0x0d0d0d0d;
+
+    const q7_t *pIn=vec_in;
 
     /* We first search for the maximum */
+    
     for (i = 0; i < dim_vec; i++)
     {
         if (vec_in[i] > base)
@@ -76,6 +89,131 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
             base = vec_in[i];
         }
     }
+
+
+    /* 
+     * So the base is set to max-8, meaning 
+     * that we ignore really small values. 
+     * anyway, they will be 0 after shrinking to q7_t.
+     */
+    base = base - (1 << 3);
+    baseV = ((base & 0x0FF) << 24) | ((base & 0x0FF) << 16) | ((base & 0x0FF) << 8) | ((base & 0x0FF));
+
+    sum = 0;
+
+    blkCnt = dim_vec >> 2;
+
+    while(blkCnt)
+    {
+       in=arm_nn_read_q7x4_ia(&pIn);
+       in=__SSUB8(in,baseV);
+
+        in1 = __SXTB16(__ROR(in, 8));
+
+        /* extend remaining two q7_t values to q15_t values */
+        in2 = __SXTB16(in);
+
+#ifndef ARM_MATH_BIG_ENDIAN
+        out2 = __PKHTB(in1, in2, 16);
+        out1 = __PKHBT(in2, in1, 16);
+#else
+        out1 = __PKHTB(in1, in2, 16);
+        out2 = __PKHBT(in2, in1, 16);
+#endif
+
+
+       shiftV = __USAT16(out1,3);
+       sum += 0x1 << (shiftV & 0x0FF);
+       sum += 0x1 << ((shiftV >> 16) & 0x0FF);
+
+       shiftV = __USAT16(out2,3);
+       sum += 0x1 << (shiftV & 0x0FF);
+       sum += 0x1 << ((shiftV >> 16) & 0x0FF);
+
+       blkCnt--;
+    }
+
+    blkCnt = dim_vec & 3;
+    
+    while(blkCnt)
+    {
+       shift = (uint8_t)__USAT(*pIn++ - base, 3);
+       sum += 0x1 << shift;
+       blkCnt--;
+    }
+
+
+    /* This is effectively (0x1 << 20) / sum */
+    int output_base = (1 << 20) / sum;
+
+
+    pIn=vec_in;
+
+    blkCnt = dim_vec >> 2;
+    while(blkCnt)
+    {
+        
+        /* Here minimum value of 13+base-vec_in[i] will be 5 */
+        in=arm_nn_read_q7x4_ia(&pIn);
+        in=__SSUB8(pad,in);
+        in=__SADD8(in,baseV);
+
+        in1 = __SXTB16(__ROR(in, 8));
+
+        /* extend remaining two q7_t values to q15_t values */
+        in2 = __SXTB16(in);
+
+#ifndef ARM_MATH_BIG_ENDIAN
+        out2 = __PKHTB(in1, in2, 16);
+        out1 = __PKHBT(in2, in1, 16);
+#else
+        out1 = __PKHTB(in1, in2, 16);
+        out2 = __PKHBT(in2, in1, 16);
+#endif
+
+        shiftV = __USAT16(out1,5);
+        *p_out++ = (q7_t) __SSAT((output_base >> (shiftV & 0x0FF)), 8);
+        *p_out++ = (q7_t) __SSAT((output_base >> ((shiftV >> 16) & 0x0FF)), 8);
+
+        shiftV = __USAT16(out2,5);
+        *p_out++ = (q7_t) __SSAT((output_base >> (shiftV & 0x0FF)), 8); 
+        *p_out++ = (q7_t) __SSAT((output_base >> ((shiftV >> 16) & 0x0FF)), 8);
+
+        blkCnt --;
+    }
+
+    
+    blkCnt = dim_vec & 3;
+    while(blkCnt)
+    {
+        
+        /* Here minimum value of 13+base-vec_in[i] will be 5 */
+        shift = (uint8_t)__USAT(13 + base - *pIn++, 5);
+        *p_out++ = (q7_t) __SSAT((output_base >> shift), 8);
+
+        blkCnt --;
+    }
+}
+#else
+void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
+{
+    q31_t     sum;
+    int16_t   i;
+    uint8_t   shift;
+    q15_t     base;
+
+    base = -128;
+
+    /* We first search for the maximum */
+    
+    for (i = 0; i < dim_vec; i++)
+    {
+        if (vec_in[i] > base)
+        {
+            base = vec_in[i];
+        }
+    }
+
 
     /* 
      * So the base is set to max-8, meaning 
@@ -105,7 +243,7 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
         
     }
 }
-
+#endif
 /**
  * @} end of Softmax group
  */

CMSIS/DSP/Testing/PatternGeneration/Softmax.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 import scipy.special as sp
 
-NBTESTSAMPLES = 100
+NBTESTSAMPLES = 500
 
 def softmax(v):
   m = sp.softmax(v)
@@ -46,7 +46,7 @@ def writeTest(config,nb,vecDim):
 
 
 def writeTests(config):
-    writeTest(config,1,15)
+    writeTest(config,1,20)
 
 
 PATTERNDIR = os.path.join("Patterns","NN","Softmax",)

CMSIS/DSP/Testing/Source/Softmax.cpp
@@ -57,11 +57,13 @@ int16_t differences(int16_t *pa,int16_t *pb, int length)
           *pOut++ = maxIndex;
 
           vec_in += this->vecDim;
+          pTmp += this->vecDim;
        }
 
        int diff = differences(ref.ptr(),output.ptr(),this->nbSamples);
-       // 5% of errors are accepted
-       ASSERT_TRUE(100.0*diff/this->nbSamples <= 5);
+       //printf("diffs = %d\n",diff);
+       // 6% of errors are accepted for 20 entry samples
+       ASSERT_TRUE(100.0*diff/this->nbSamples <= 6);
        
     } 
 
@@ -85,13 +87,17 @@ int16_t differences(int16_t *pa,int16_t *pb, int length)
        }
 
         output.create(ref.nbSamples(),Softmax::OUTPUT_S16_ID,mgr);
-        temp.create(this->vecDim,Softmax::TEMP_Q7_ID,mgr);
+        // Used to compare bit exactness of the reference C version
+        // and the optimized version.
+        temp.create(this->vecDim*this->nbSamples,Softmax::TEMP_Q7_ID,mgr);
        
 
     }
 
     void Softmax::tearDown(Testing::testID_t id,Client::PatternMgr *mgr)
     {
-        output.dump(mgr);
+        // Array are big so by default they are not dumped and only
+        // used for debug.
+        //output.dump(mgr);
         //temp.dump(mgr);
     }

commit 49dc305607be32eca1f3ade3553283efac7d9122

CMSIS-NN : Revert update to arm_softmax_q7()
To avoid accuracy issues caused by earlier commits ede5bd3e2 and
cebb96c3f, the changes are reverted.

Change-Id: I59977d130486710fa569b499a71d11f2fee74fa3

CMSIS/NN/Source/SoftmaxFunctions/arm_softmax_q7.c
@@ -21,7 +21,7 @@
  * Title:        arm_softmax_q7.c
  * Description:  Q7 softmax function
  *
- * $Date:        February 27, 2020
+ * $Date:        June 8, 2020
  * $Revision:    V.1.0.1
  *
  * Target Processor:  Cortex-M cores
@@ -57,49 +57,17 @@
    *  But mathematically, the gradient will be the same
    *  with a log(2) scaling factor.
    *
-   *  If we compare the position of the max value in output of this
-   *  function with a reference float32 softmax (and thus using exp)
-   *  we see that the position of the max value is sometimes different.
-   *
-   *  If we do statistics on lot of input vectors we can compute
-   *  an average error rate in percent. It is the percent of time
-   *  that the max will be at a position different from the one
-   *  computed with a reference float32 implementation.
-   *
-   *  This average error rate is dependent on the vector size.
-   *  We have:
-   *
-   *  Average error rate in percent = -0.555548 + 0.246918 dim_vec
-   *  Variance of the error rate = -0.0112281 + 0.0382476 dim_vec
-   *
-   *
    */
-
-#define Q7BITS 8
-#define LOG2Q7BITS 3
 
 void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
 {
-#if defined (ARM_MATH_DSP)
     q31_t     sum;
     int16_t   i;
     uint8_t   shift;
     q15_t     base;
-    uint16_t blkCnt;
-
-    q31_t in,in1,in2;
-    q31_t out1, out2;
-
-    q31_t baseV;
-    q31_t shiftV;
-    const q31_t pad=0x0d0d0d0d;
-    const q7_t *pIn=vec_in;
-
     base = -128;
 
-
     /* We first search for the maximum */
-
     for (i = 0; i < dim_vec; i++)
     {
         if (vec_in[i] > base)
@@ -108,157 +76,33 @@ void arm_softmax_q7(const q7_t * vec_in, const uint16_t dim_vec, q7_t * p_out )
         }
     }
 
-
     /*
      * So the base is set to max-8, meaning
      * that we ignore really small values.
      * anyway, they will be 0 after shrinking to q7_t.
      */
-    base = base - Q7BITS;
-    baseV = ((base & 0x0FF) << 24) | ((base & 0x0FF) << 16) | ((base & 0x0FF) << 8) | ((base & 0x0FF));
-
-    sum = 0;
-
-    blkCnt = dim_vec >> 2;
-
-    while(blkCnt)
-    {
-       in=arm_nn_read_q7x4_ia(&pIn);
-       in=__SSUB8(in,baseV);
-
-        in1 = __SXTB16(__ROR(in, 8));
-
-        /* extend remaining two q7_t values to q15_t values */
-        in2 = __SXTB16(in);
-
-#ifndef ARM_MATH_BIG_ENDIAN
-        out2 = __PKHTB(in1, in2, 16);
-        out1 = __PKHBT(in2, in1, 16);
-#else
-        out1 = __PKHTB(in1, in2, 16);
-        out2 = __PKHBT(in2, in1, 16);
-#endif
-
-
-       shiftV = __USAT16(out1,LOG2Q7BITS);
-       sum += 0x1 << (shiftV & 0x0FF);
-       sum += 0x1 << ((shiftV >> 16) & 0x0FF);
-
-       shiftV = __USAT16(out2,LOG2Q7BITS);
-       sum += 0x1 << (shiftV & 0x0FF);
-       sum += 0x1 << ((shiftV >> 16) & 0x0FF);
-
-       blkCnt--;
-    }
-
-    blkCnt = dim_vec & 3;
-
-    while(blkCnt)
-    {
-       shift = (uint8_t)__USAT(*pIn++ - base, LOG2Q7BITS);
-       sum += 0x1 << shift;
-       blkCnt--;
-    }
-
-
-    /* This is effectively (0x1 << 20) / sum */
-    int output_base = (1 << 20) / sum;
-
-
-    pIn=vec_in;
-
-    blkCnt = dim_vec >> 2;
-    while(blkCnt)
-    {
-
-        /* Here minimum value of 13+base-vec_in[i] will be 5 */
-        in=arm_nn_read_q7x4_ia(&pIn);
-        in=__SSUB8(pad,in);
-        in=__SADD8(in,baseV);
-
-        in1 = __SXTB16(__ROR(in, 8));
-
-        /* extend remaining two q7_t values to q15_t values */
-        in2 = __SXTB16(in);
-
-#ifndef ARM_MATH_BIG_ENDIAN
-        out2 = __PKHTB(in1, in2, 16);
-        out1 = __PKHBT(in2, in1, 16);
-#else
-        out1 = __PKHTB(in1, in2, 16);
-        out2 = __PKHBT(in2, in1, 16);
-#endif
-
-        shiftV = __USAT16(out1,5);
-        *p_out++ = (q7_t) __SSAT((output_base >> (shiftV & 0x0FF)), 8);
-        *p_out++ = (q7_t) __SSAT((output_base >> ((shiftV >> 16) & 0x0FF)), 8);
-
-        shiftV = __USAT16(out2,5);
-        *p_out++ = (q7_t) __SSAT((output_base >> (shiftV & 0x0FF)), 8);
-        *p_out++ = (q7_t) __SSAT((output_base >> ((shiftV >> 16) & 0x0FF)), 8);
-
-        blkCnt --;
-    }
-
-
-    blkCnt = dim_vec & 3;
-    while(blkCnt)
-    {
-
-        /* Here minimum value of 13+base-vec_in[i] will be 5 */
-        shift = (uint8_t)__USAT(13 + base - *pIn++, 5);
-        *p_out++ = (q7_t) __SSAT((output_base >> shift), 8);
-
-        blkCnt --;
-    }
-#else
-    q31_t     sum;
-    int16_t   i;
-    uint8_t   shift;
-    q15_t     base;
-
-    base = -128;
-
-    /* We first search for the maximum */
-
-    for (i = 0; i < dim_vec; i++)
-    {
-        if (vec_in[i] > base)
-        {
-            base = vec_in[i];
-        }
-    }
-
-
-    /*
-     * So the base is set to max-8, meaning
-     * that we ignore really small values.
-     * anyway, they will be 0 after shrinking to q7_t.
-     */
-    base = base - Q7BITS;
+    base = base - (1 << 3);
 
     sum = 0;
 
     for (i = 0; i < dim_vec; i++)
     {
-        shift = (uint8_t)__USAT(vec_in[i] - base, LOG2Q7BITS);
+        shift = (uint8_t)__USAT(vec_in[i] - base, 3);
         sum += 0x1 << shift;
     }
 
     /* This is effectively (0x1 << 20) / sum */
     int output_base = (1 << 20) / sum;
 
-
     for (i = 0; i < dim_vec; i++)
     {
 
         /* Here minimum value of 13+base-vec_in[i] will be 5 */
         shift = (uint8_t)__USAT(13 + base - vec_in[i], 5);
-        p_out[i] = (q7_t) __SSAT((output_base >> shift), 8);
-
+        p_out[i] = (q7_t)__SSAT((output_base >> shift), 8);
     }
-#endif
 }
+
 /**
  * @} end of Softmax group
  */