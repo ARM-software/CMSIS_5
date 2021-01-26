1295685d6c8b6e4c8c35dd5c7b6e8ed9ed8d9d9f
Jonatan Antoni <jonatan.antoni@arm.com>
2021-01-26 15:23:01 +0100

    CoreValidation: Updat build config and script. - Add new build.py using python-matrix-builder module. - Rename model configs to reflect build target. - Add Cortex-M55 build targets. - Clang/GCC use compiler features instead of -mfpu explicitly - Remove disregarded flags

--- CMSIS/CoreValidation/Tests/RTE_Components.h
+++ CMSIS/CoreValidation/Tests/RTE_Components.h
@@ -33,6 +33,8 @@
 #define CMSIS_device_header "ARMCM35P.h"
 #elif defined(ARMCM35P_DSP_FP_TZ)
 #define CMSIS_device_header "ARMCM35P_DSP_FP_TZ.h"
+#elif defined(ARMCM55)
+#define CMSIS_device_header "ARMCM55.h"
 #elif defined(ARMCA5)
 #define CMSIS_device_header "ARMCA5.h"
 #elif defined(ARMCA7)
