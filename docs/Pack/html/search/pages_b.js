20285262657d1b482d132d20d755c8c330d55c1f
Christopher Seidl <KeilChris@users.noreply.github.com>
2021-02-01 16:56:13 +0100

    Updated CMSIS-Pack documentation
    Tutorials rewritten and adapted to new glow including gen_pack.sh.

--- docs/Pack/html/search/pages_b.js
+++ docs/Pack/html/search/pages_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['xml_20validation_20and_20editors',['XML Validation and Editors',['../cp_Editors.html',1,'createPackUtil']]]
+  ['xml_20validation',['XML Validation',['../cp_Editors.html',1,'createPackUtil']]]
 ];
