0e2c6b290cb6cbabd6a1dba65de695dbd2e22579
TTornblom <thomas.tornblom@iar.com>
2021-04-12 14:33:17 +0200

    Build: Update IAR support for 5.8.0
    Added stack sealing support to header and assembly startup files
    for cm23, cm33, cm35p. The link script support for this will be
    provided in an updated IAR CMSIS-Manager, once that has been
    released.
    
    Signed-off-by: TTornblom <thomas.tornblom@iar.com>

--- CMSIS/Core/Include/cmsis_iccarm.h
+++ CMSIS/Core/Include/cmsis_iccarm.h
@@ -267,6 +267,24 @@ __packed struct  __iar_u32 { uint32_t v; };
 #define __VECTOR_TABLE_ATTRIBUTE  @".intvec"
 #endif
 
+#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+#ifndef __STACK_SEAL
+#define __STACK_SEAL              STACKSEAL$$Base
+#endif
+
+#ifndef __TZ_STACK_SEAL_SIZE
+#define __TZ_STACK_SEAL_SIZE      8U
+#endif
+
+#ifndef __TZ_STACK_SEAL_VALUE
+#define __TZ_STACK_SEAL_VALUE     0xFEF5EDA5FEF5EDA5ULL
+#endif
+
+__STATIC_FORCEINLINE void __TZ_set_STACKSEAL_S (uint32_t* stackTop) {
+  *((uint64_t *)stackTop) = __TZ_STACK_SEAL_VALUE;
+}
+#endif
+
 #ifndef __ICCARM_INTRINSICS_VERSION__
   #define __ICCARM_INTRINSICS_VERSION__  0
 #endif
