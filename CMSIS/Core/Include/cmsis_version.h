4a62731db42586b8c0d5094cc426ae813913b94b
Jonatan Antoni <jonatan.antoni@arm.com>
2021-04-28 12:37:12 +0200

    Dummy change for kicking CI build.

--- CMSIS/Core/Include/cmsis_version.h
+++ CMSIS/Core/Include/cmsis_version.h
@@ -32,8 +32,8 @@
 #define __CMSIS_VERSION_H
 
 /*  CMSIS Version definitions */
-#define __CM_CMSIS_VERSION_MAIN  ( 5U)                                      /*!< [31:16] CMSIS Core(M) main version */
-#define __CM_CMSIS_VERSION_SUB   ( 4U)                                      /*!< [15:0]  CMSIS Core(M) sub version */
+#define __CM_CMSIS_VERSION_MAIN  ( 6U)                                      /*!< [31:16] CMSIS Core(M) main version */
+#define __CM_CMSIS_VERSION_SUB   ( 0U)                                      /*!< [15:0]  CMSIS Core(M) sub version */
 #define __CM_CMSIS_VERSION       ((__CM_CMSIS_VERSION_MAIN << 16U) | \
                                    __CM_CMSIS_VERSION_SUB           )       /*!< CMSIS Core(M) version number */
 #endif
