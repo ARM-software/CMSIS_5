63baf184b2806f5370bd3368ac2f5b93dbced006
TTornblom <thomas.tornblom@iar.com>
2021-09-21 16:36:20 +0200

    DSP: Cleanup of some non-standard C
    In preparation for IAR support, this is some cleanup of
    non-standard gcc-isms and a few added defines for IAR.
    
    Signed-off-by: TTornblom <thomas.tornblom@iar.com>

--- CMSIS/DSP/Include/arm_math_types_f16.h
+++ CMSIS/DSP/Include/arm_math_types_f16.h
@@ -51,7 +51,7 @@ won't be built.
 */
 #if !(__ARM_FEATURE_MVE & 2)
   #if !defined(DISABLEFLOAT16)
-    #if defined(__ARM_FP16_FORMAT_IEEE) || defined(__ARM_FP16_FORMAT_ALTERNATIVE)
+    #if defined(__ARM_FP16_FORMAT_IEEE) || defined(__ARM_FP16_FORMAT_ALTERNATIVE) || defined(___FLOAT16_SIZE__)
       typedef __fp16 float16_t;
       #define ARM_FLOAT16_SUPPORTED
     #endif
@@ -62,7 +62,7 @@ won't be built.
 #endif
 
 #if defined(ARM_MATH_NEON) || (defined(ARM_MATH_MVEF)  && !defined(ARM_MATH_AUTOVECTORIZE)) /* floating point vector*/
-  
+
 #if defined(ARM_MATH_MVE_FLOAT16) || defined(ARM_MATH_NEON_FLOAT16)
 
   /**
@@ -93,7 +93,7 @@ won't be built.
 #endif
 
 #if defined(ARM_MATH_NEON)
- 
+
 
 #if defined(ARM_MATH_NEON_FLOAT16)
   /**
@@ -129,21 +129,31 @@ won't be built.
       float16x4_t     f;
       int16x4_t       i;
   } any16x4_t;
-#endif 
+#endif
 
 #endif
 
 
 
 #if defined(ARM_FLOAT16_SUPPORTED)
+
+#if defined(__ICCARM__) && !defined(__FLT16_MAX__)
+
+#define __FLT16_MAX__ (float16_t) 0x7bff
+#define F16INFINITY (float16_t) 0x7c00
+
+#else
+
+#define F16INFINITY ((float16_t)__builtin_inf())
+
+#endif
+
 #define F16_MAX   ((float16_t)__FLT16_MAX__)
 #define F16_MIN   (-(_Float16)__FLT16_MAX__)
 
 #define F16_ABSMAX   ((float16_t)__FLT16_MAX__)
 #define F16_ABSMIN   ((float16_t)0.0f16)
 
-#define F16INFINITY ((float16_t)__builtin_inf())
-  
 #endif /* ARM_FLOAT16_SUPPORTED*/
 #endif /* !defined( __CC_ARM ) */
 
@@ -152,5 +162,3 @@ won't be built.
 #endif
 
 #endif /* _ARM_MATH_F16_H */
-
-
