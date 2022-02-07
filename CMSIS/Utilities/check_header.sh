67a069ed5157f271dc680dd5a8dbcb92123ecb36
Jonatan Antoni <jonatan.antoni@arm.com>
2022-02-07 14:54:40 +0100

    GitHub: Fix fileheader workflow
    Calculating the change dates of files in a pull-request
    requires history of all commits.

--- CMSIS/Utilities/check_header.sh
+++ CMSIS/Utilities/check_header.sh
@@ -16,8 +16,8 @@ fi
 FILE_VERSION=$(grep -E '@version\s+V?([0-9]+\.[0-9]+(\.[0-9]+)?)' ${FILE} | sed -E 's/^.*@version\s+V?([0-9]+\.[0-9]+(\.[0-9]+)?).*/\1/')
 if [[ ! -z $FILE_VERSION ]]; then
   FILE_VERSION_LINE=$(grep -En "@version.*${FILE_VERSION}" ${FILE} | cut -f1 -d:)
-  AUTHOR_REV=$(git log -1 --pretty="format:%H")
-  PARENT_REV=$(git log -1 --pretty="format:%P")
+  AUTHOR_REV=$(git log -1 --pretty="format:%H" -- ${FILE})
+  PARENT_REV=$(git log -1 --pretty="format:%P" -- ${FILE})
   BLAME_REV=$(git blame ${PARENT_REV}..${AUTHOR_REV} -l -L ${FILE_VERSION_LINE},${FILE_VERSION_LINE} ${FILE} | sed -E 's/^([[:alnum:]]+).*/\1/')
   if [[ $AUTHOR_REV != $BLAME_REV ]]; then
     echo "${FILE}:${FILE_VERSION_LINE}:Please increment file version." >&2
