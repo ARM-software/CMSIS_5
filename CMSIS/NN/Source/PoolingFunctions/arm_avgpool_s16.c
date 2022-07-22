ab10c26cd17c48b2cb01254f1967303d890d9ade
Felix Thomasmathibalan <felixjohnny.thomasmathibalan@arm.com>
2022-07-22 11:00:39 +0200

    CMSIS-NN : MVE int16 average pooling support
    MVE optimization is added for int16 average pooling.
    
    New test cases are added to increase code coverage
    
    Change-Id: Ic7e2004de9cc1244246e721ca02085f6ff489018

--- CMSIS/NN/Source/PoolingFunctions/arm_avgpool_s16.c
+++ CMSIS/NN/Source/PoolingFunctions/arm_avgpool_s16.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Arm Limited or its affiliates.
+ * SPDX-FileCopyrightText: Copyright 2022 Arm Limited and/or its affiliates <open-source-office@arm.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -21,8 +21,8 @@
  * Title:        arm_avgpool_s16.c
  * Description:  Pooling function implementations
  *
- * $Date:        17 May 2022
- * $Revision:    V.2.1.0
+ * $Date:        27 July 2022
+ * $Revision:    V.2.2.0
  *
  * Target Processor:  Cortex-M CPUs
  *
@@ -91,8 +91,94 @@ arm_cmsis_nn_status arm_avgpool_s16(const cmsis_nn_context *ctx,
     const int32_t act_min = pool_params->activation.min;
     const int32_t act_max = pool_params->activation.max;
     const int32_t ch_src = input_dims->c;
+#if defined(ARM_MATH_MVEI)
+    (void)ctx;
+    for (int i_y = 0; i_y < output_y; i_y++)
+    {
+        for (int i_x = 0; i_x < output_x; i_x++)
+        {
+            const int32_t k_y_start = MAX(0, i_y * stride_y - pad_y);
+            const int32_t k_y_end = MIN(i_y * stride_y - pad_y + kernel_y, input_y);
 
-#if defined(ARM_MATH_DSP) && !defined(ARM_MATH_MVEI)
+            const int32_t k_x_start = MAX(0, i_x * stride_x - pad_x);
+            const int32_t k_x_end = MIN(i_x * stride_x - pad_x + kernel_x, input_x);
+
+            const int16_t *src_base = src;
+            int16_t *out = &dst[ch_src * (i_x + i_y * output_x)];
+
+            int32_t ch_count = (ch_src + 7) / 8;
+            int32_t channels = ch_src;
+
+            while (ch_count > 0)
+            {
+                int32_t count = 0;
+
+                int32x4_t sum_1 = vdupq_n_s32(0);
+                int32x4_t sum_2 = vdupq_n_s32(0);
+                // Load store tail predicate
+                const mve_pred16_t ld_st_p = vctp16q(channels);
+                channels -= 8;
+
+                for (int k_y = k_y_start; k_y < k_y_end; k_y++)
+                {
+                    for (int k_x = k_x_start; k_x < k_x_end; k_x++)
+                    {
+                        const int16_t *src_inner = src_base + (ch_src * (k_x + k_y * input_x));
+                        const int16x8_t temp = vldrhq_z_s16(src_inner, ld_st_p);
+
+                        const int32x4_t temp_lo = vmovlbq_s16(temp);
+                        const int32x4_t temp_hi = vmovltq_s16(temp);
+
+                        sum_1 = vaddq_s32(sum_1, temp_lo);
+                        sum_2 = vaddq_s32(sum_2, temp_hi);
+
+                        count++;
+                    }
+                }
+
+                // Prevent static code issue DIVIDE_BY_ZERO.
+                if (count == 0)
+                {
+                    return ARM_CMSIS_NN_ARG_ERROR;
+                }
+
+                // Perform the following operation
+                // sum = sum > 0 ? (sum + count / 2) / count : (sum - count / 2) / count;
+                const int32_t half_count = count / 2;
+                // Predicate for 'sum > 0' operation
+                mve_pred16_t p = vcmpgtq_n_s32(sum_1, 0);
+                sum_1 = vaddq_m_n_s32(sum_1, sum_1, half_count, p);
+                sum_1 = vsubq_m_n_s32(sum_1, sum_1, half_count, ~p);
+
+                p = vcmpgtq_n_s32(sum_2, 0);
+                sum_2 = vaddq_m_n_s32(sum_2, sum_2, half_count, p);
+                sum_2 = vsubq_m_n_s32(sum_2, sum_2, half_count, ~p);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    sum_1[i] = sum_1[i] / count;
+                    sum_2[i] = sum_2[i] / count;
+                }
+
+                sum_1 = vmaxq_s32(sum_1, vdupq_n_s32(act_min));
+                sum_1 = vminq_s32(sum_1, vdupq_n_s32(act_max));
+
+                sum_2 = vmaxq_s32(sum_2, vdupq_n_s32(act_min));
+                sum_2 = vminq_s32(sum_2, vdupq_n_s32(act_max));
+
+                int16x8_t temp = vdupq_n_s16(0);
+                temp = vmovnbq_s32(temp, sum_1);
+                temp = vmovntq_s32(temp, sum_2);
+
+                vstrhq_p_s16(out, temp, ld_st_p);
+
+                out += 8;
+                ch_count--;
+                src_base += 8;
+            }
+        }
+    }
+#elif defined(ARM_MATH_DSP)
 
     q31_t *buffer = (q31_t *)ctx->buf;
 
